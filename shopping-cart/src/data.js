ef39e52e58ec86f544e217a2d75d75a5b6a5fae8
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-28 15:44:18 +0200

    first commit

---
+++ shopping-cart/src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Macbook',
+            price: 1400,
+            image: 'https://picsum.photos/is/180/2400/1600'
+        },
+        {
+            id: '2',
+            name: 'Old Car',
+            price: 2400,
+            image: 'https://picsum.photos/is/111/4400/2656'
+        },
+        {
+            id: '3',
+            name: 'White Shoes',
+            price: 1000,
+            image: 'https://picsum.photos/is/21/3008/2008'
+        }
+    ]
+}
+
+export default data;
